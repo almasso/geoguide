7e0b44a37bc494e9d332e06d2422b1ae7de19599
almasso <almasso@ucm.es>
2023-10-17 20:34:05 +0200

    Cubo que se transforma en bola

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+//Sacado del vídeo de Sebastian Lague acerca de la generación procedural de planetas (https://youtu.be/QN39W020LqU?si=bPH1h8OAmpn7sDmF)
+
+//Nuestra clase Planet va a crear los planetas como un cubo con 6 meshes, cada una apuntando en una dirección distinta. Posteriormente, usando matemáticas,
+//haremos que cada uno de esos meshes se curve y se forme una esfera (haciendo que los vértices estén todos a la misma distancia del centro del cubo).
+//A mayor resolución tengamos en nuestros TerrainFaces, más continua aparecerá la superficie una vez se curve.
+public class Planet : MonoBehaviour {
+
+    [SerializeField] [Range(2,256)] int resolution = 10;
+
+    [SerializeField, HideInInspector] MeshFilter[] meshFilters;
+    TerrainFace[] faces;
+
+    private void OnValidate() {
+        Inicializar();
+        GenerarMesh();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Inicializar() {
+        if(meshFilters == null || meshFilters.Length == 0) meshFilters = new MeshFilter[6];
+        faces = new TerrainFace[6];
+        Vector3[] direcciones = { Vector3.up, Vector3.down, Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
+
+        for(int i = 0; i < meshFilters.Length; i++) {
+            if (meshFilters[i] == null) {
+                GameObject meshObject = new GameObject("mesh");
+                meshObject.transform.parent = transform;
+
+                meshObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObject.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+            faces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, direcciones[i]);
+        }
+    }
+
+    void GenerarMesh() {
+        foreach(TerrainFace t in faces) {
+            t.ConstruirMesh();
+        }
     }
 }
--- Assets/Scripts/TerrainFaceScript.cs
+++ Assets/Scripts/TerrainFaceScript.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Sacado del vídeo de Sebastian Lague acerca de la generación procedural de planetas (https://youtu.be/QN39W020LqU?si=bPH1h8OAmpn7sDmF)
+
+//Esta TerrainFace va a ser una de las 6 que compongan el cubo que va a ser nuestro planeta. Posteriormente, en el script "Planet" definiremos 6
+//TerrainFaces, cada una con una orientación distinta, y es cuando las "curvaremos" para que en conjunto formen un cilindro en el que todos los triángulos
+//de las caras en los Mesh que lo componen tengan el mismo tamaño.
 public class TerrainFace {
 
-    Mesh mesh; //Mesh
-    int resolution; //Resolución de la mesh
+    Mesh mesh; //Mesh que construimos para la cara
+    int resolution; //Resolución de la mesh, es decir, el número de detalle (a más detalle, más triángulos se generan en la mesh)
+    
     //Vectores que definen el plano de la mesh
-    Vector3 localUp;
-    Vector3 axisA;
-    Vector3 axisB;
+    Vector3 localUp; //Este es el vector en el que la mesh está mirando
+    //Estos dos vectores formarían el espacio vectorial de nuestro plano
+    Vector3 vectorDirectorA;
+    Vector3 vectorDirectorB;
 
     //Constructora de TerrainFace en la que se guardan los valores pasados y se calculan los otros dos vectores
     public TerrainFace(Mesh mesh, int resolution, Vector3 localUp) {
@@ -17,40 +24,40 @@ public class TerrainFace {
         this.resolution = resolution;
         this.localUp = localUp;
 
-        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
-        axisB = Vector3.Cross(localUp, axisA);
+        vectorDirectorA = new Vector3(localUp.y, localUp.z, localUp.x); //Para generar el vectorDirectorA simplemente mezclamos las coordenadas de nuestro vector en el que mira la mesh
+        vectorDirectorB = Vector3.Cross(localUp, vectorDirectorA); //Y para generar el otro vector, simplemente hacemos el producto vectorial entre los dos vectores de nuestro espacio vectorial (ya que tiene que ser perpendicular a ambos)
     }
 
 
-    public void ConstructMesh()
-    {
-        Vector3[] vertices = new Vector3[resolution * resolution];
-        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
-        int indexTriangles = 0;
-        for (int i = 0; i < resolution; ++i)
-        {
-            for(int j = 0; j < resolution; ++j)
-            {
-                int k = i + j * resolution;
-                Vector2 percent = new Vector2(i, j) / (resolution - 1);
-                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
-                vertices[k] = pointOnUnitCube;
+    public void ConstruirMesh() {
+        Vector3[] vertices = new Vector3[resolution * resolution]; //Generamos un vector de vértices de tamaño resolución^2, ya que la resolución es el número de vértices que vamos a tener a lo largo de uno de los ejes de la mesh
+        int[] indicesTriangulos = new int[(resolution - 1) * (resolution - 1) * 6]; //Dado una mesh de tamaño r*r en resolución, tendremos en total (r-1)^2 cuadrados en la mesh (de una cuadrícula de 2*2 vértices, tendremos solo 1 cuadrado con sus 4 vértices).
+        //Ahora dividimos esos cuadrados por su diagonal en 2 triángulos. Tendremos un total de (r-1)^2 * 2 triángulos en toda la malla. Como cada triángulo tiene 3 vértices, tendremos un total de (r-1)^2 * 2 * 3 índices para nuestros triángulos.
+        int indiceTrianguloActual = 0;
+        int k = 0;
+        for (int i = 0; i < resolution; ++i) {
+            for(int j = 0; j < resolution; ++j) {
+                Vector2 percent = new Vector2(j, i) / (resolution - 1); //Este vector nos sirve para ver por qué por ciento van los bucles for en cada uno de los ejes.
+                Vector3 puntoEnLaMesh = localUp + (percent.x - 0.5f) * 2 * vectorDirectorA + (percent.y - 0.5f) * 2 * vectorDirectorB; //Esto calcula el vector de posición de nuestro vértice en la mesh dado nuestro vector localUp, que empieza en el origen que nosotros establezcamos
+                Vector3 puntoEnLaMeshEsferica = puntoEnLaMesh.normalized;
+                vertices[k] = puntoEnLaMeshEsferica;
 
-                if(i != resolution - 1 && j != resolution - 1)
-                {
-                    triangles[indexTriangles] = k;
-                    triangles[indexTriangles + 1] = k + resolution + 1;
-                    triangles[indexTriangles + 2] = k + resolution;
-                    triangles[indexTriangles + 3] = k;
-                    triangles[indexTriangles + 4] = k + resolution;
-                    triangles[indexTriangles + 5] = k + resolution + 1;
-                    indexTriangles += 6;
+                if(i != resolution - 1 && j != resolution - 1) {
+                    //Definimos los triángulos en la mesh en sentido horario excepto para la última coordenada de los ejes X e Y
+                    indicesTriangulos[indiceTrianguloActual] = k;
+                    indicesTriangulos[indiceTrianguloActual + 1] = k + resolution + 1;
+                    indicesTriangulos[indiceTrianguloActual + 2] = k + resolution;
+                    indicesTriangulos[indiceTrianguloActual + 3] = k;
+                    indicesTriangulos[indiceTrianguloActual + 4] = k + 1;
+                    indicesTriangulos[indiceTrianguloActual + 5] = k + resolution + 1;
+                    indiceTrianguloActual += 6;
                 }
+                ++k;
             }
         }
-        mesh.Clear();
+        mesh.Clear(); //Limpiamos la data del mesh para cuando bajemos la resolución no tengamos triángulos inexistentes que apunten a vértices inexistentes, sino que lo vuelva a recalcular todo de nuevo
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.triangles = indicesTriangulos;
         mesh.RecalculateNormals();
     }
    
