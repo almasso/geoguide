77b6df2790082c76052b670ffcfe76c61293c5b9
almasso <almasso@ucm.es>
2023-11-16 15:03:55 +0100

    Nueva cámara limpia :)

---
+++ Assets/Scripts/AlexScripts/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CameraManager;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject _plane;
+    [Header("Opciones de la camara")]
+    [SerializeField] private AjustesCamara _ajustes;
+
+    private Camera _mainCamera;
+    private Transform _playerTransform;
+
+
+    [System.Serializable]
+    public struct AjustesCamara {
+        public Vector3 offset;
+        public Vector3 lookAtOffset;
+        public AjustesCamara(Vector3 offset, Vector3 lookAtOffset)
+        {
+            this.offset = offset;
+            this.lookAtOffset = lookAtOffset;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+        _playerTransform = _plane.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mainCamera.GetComponent<Transform>().position = _playerTransform.position + _playerTransform.forward * _ajustes.offset.z + _playerTransform.position * _ajustes.offset.y;
+
+        Vector3 lookTarget = _playerTransform.position;
+        lookTarget += _playerTransform.right * _ajustes.lookAtOffset.x;
+        lookTarget += _playerTransform.up * _ajustes.lookAtOffset.y;
+        lookTarget += _playerTransform.forward * _ajustes.lookAtOffset.z;
+        _mainCamera.GetComponent<Transform>().LookAt(lookTarget, _playerTransform.position);
+    }
+}
--- Assets/Scripts/AlexScripts/PlayerController.cs
+++ Assets/Scripts/AlexScripts/PlayerController.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Transform _planetTransform;
+    [SerializeField] private GameObject _planetToOrbit;
+    [SerializeField] private GameObject _rotationNode;
+    private Transform _pTOTransform;
+    [Header("Ajustes del avion")]
+    [SerializeField] private float rollSpeed = 0.01f;
+    [SerializeField] private float yawSpeed;
+    [SerializeField] private Vector3 _orbitOffset;
+
     private Transform _planeTransform;
     private Transform _planeNodeTransform;
-    private SphereCollider _planetaCollider;
-    private Camera _mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        _mainCamera = Camera.main;
-        _planetTransform = GameObject.Find("Planeta").GetComponent<Transform>();
-        _planetaCollider = GameObject.Find("Planeta").GetComponent<SphereCollider>();
-        _planeNodeTransform = GameObject.Find("PlayerNode").GetComponent<Transform>();
-
+        _pTOTransform = _planetToOrbit.GetComponent<Transform>();
         _planeTransform = this.GetComponent<Transform>();
-        _planeTransform.position = _planetTransform.position + new Vector3(0, 350, 0);
-        _mainCamera.GetComponent<Transform>().position = _planeTransform.position;
+        _planeTransform.position = _planetToOrbit.GetComponent<Transform>().position + new Vector3(0, _pTOTransform.localScale.y / 2, 0) + _orbitOffset;
+        _planeNodeTransform = _rotationNode.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //_planeTransform.position = _planetTransform.position + new Vector3(0, 350, 0);
-        _planeNodeTransform.Rotate(new Vector3(1, 0, 0));
-        _mainCamera.GetComponent<Transform>().position = _planeTransform.position;
+        _planeNodeTransform.Rotate(new Vector3(rollSpeed, 0, 0));
     }
 }
