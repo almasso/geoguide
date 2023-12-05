8d09ac044280386122e696b71a13013dd7ce0479
almasso <almasso@ucm.es>
2023-12-05 14:13:49 +0100

    Revert "Intento número 67888+989861654456465 de brújula (basta)"
    This reverts commit 6e5d654fc8cdcfdf4589034ed8419aeefe84acc0.

--- Assets/Scripts/AlexScripts/CompassController.cs
+++ Assets/Scripts/AlexScripts/CompassController.cs
@@ -5,29 +5,23 @@ using UnityEngine.UI;
 
 public class CompassController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private GameObject _planeNose;
-    [SerializeField] private GameObject _northPole;
-    [SerializeField] private GameObject _planeCenter;
 
-    private Transform noseTransform, NpoleTransform, centerTransform, compassTransform;
+    [SerializeField] private GameObject _avionNariz;
+    [SerializeField] private GameObject _northPole;
+
+    private Transform _myTransform, _avionNarizTransform;
+    // Start is called before the first frame update
     void Start()
     {
-        noseTransform = _planeNose.transform;
-        NpoleTransform = _northPole.transform;
-        centerTransform = _planeCenter.transform;
-        compassTransform = this.transform;
+        _myTransform = this.GetComponent<Transform>();
+        _avionNarizTransform = _avionNariz.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 northPoleToPlaneCenter = (NpoleTransform.position - centerTransform.position).normalized;
-        Vector3 planeCenterToPlaneNose = (centerTransform.position - noseTransform.position).normalized;
-
-        float angle = Vector3.SignedAngle(northPoleToPlaneCenter, planeCenterToPlaneNose, Vector3.forward);
-
-        Debug.Log(angle);
-        compassTransform.eulerAngles = new Vector3(0, 0, -angle);
+        Vector3 dir = (_avionNarizTransform.position - _northPole.transform.position).normalized;
+        Quaternion targetRot = Quaternion.LookRotation(dir, Vector3.up);
+        _myTransform.Rotate(0,0,targetRot.y);
     }
 }
