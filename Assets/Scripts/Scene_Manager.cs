0d31c8a704fc66a0e031bc64a139385a9a903555
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2023-12-20 18:56:56 +0100

    correccion

--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -40,13 +40,13 @@ public class Scene_Manager : MonoBehaviour
     {
         refreshData();
         SceneManager.LoadScene("LevelsScene");
-        IndexController.namePreviousScene = "LevelScene";
+        IndexController.namePreviousScene = "LevelsScene";
     }
 
     public void StartGamePause()
     {
         SceneManager.LoadScene("LevelsScene");
-        IndexController.namePreviousScene = "LevelScene";
+        IndexController.namePreviousScene = "LevelsScene";
     }
 
     public void PauseGame()
@@ -79,7 +79,7 @@ public class Scene_Manager : MonoBehaviour
     public void BackToMainMenu()
     {
         refreshData();
-        IndexController.namePreviousScene = "LevelScene";
+        IndexController.namePreviousScene = "LevelsScene";
         SceneManager.LoadScene("MainMenuScene");
     }
     public void Tarjetasmenu()
